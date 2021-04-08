55a40971e2eab30f9001f84df66d43e1367f114c
Michael Beckford <tavonbeckford@gmail.com>
2021-04-07 20:51:30 -0500

    Updated Lab Files

--- app/views.py
+++ app/views.py
@@ -5,12 +5,29 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import os
 from app import app
-from flask import render_template, request
+from flask import render_template, request, jsonify
+from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 
 ###
 # Routing for your application.
 ###
+
+
+
+@app.route("/api/upload", methods=["POST"])
+def upload():
+    form = UploadForm()
+    if request.method == "POST":
+        if form.validate_on_submit() == True:
+            descrip = form.description.data
+            filename = assignPath(form.photo.data)
+            return jsonify(message="File uploaded successfully", filename=filename, description=descrip)
+        else:
+            return jsonify(errors=form_errors(form))
+
 
 
 # Please create all new routes and view functions above this route.
@@ -44,6 +61,13 @@ def form_errors(form):
 
     return error_messages
 
+#Saves the uploaded photo to a folder
+def assignPath(upload):
+    filename = secure_filename(upload.filename)
+    upload.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename
+    ))
+    return filename
 
 ###
 # The functions below should be applicable to all Flask apps.
